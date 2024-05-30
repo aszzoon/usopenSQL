da9e0047423b36d8e6433b8e415bff791f706058
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2024-05-30 20:18:18 +0900

    commit

---
+++ queryTrainning.sql
@@ -0,0 +1,12 @@
+use study;
+
+CREATE TABLE tCity
+(
+    name CHAR(10) PRIMARY KEY,
+    area INT NULL ,
+    popu INT NULL ,
+    metro CHAR(1) NOT NULL,
+    region CHAR(6) NOT NULL
+);
+
+show tables;
